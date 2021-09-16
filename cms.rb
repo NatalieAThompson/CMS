8c3ec712802a298761fb61ceff2f7cde392a58a7
Natalie Thompson <NatalieAThompson@outlook.com>
2021-09-16 18:09:05 -0500

    Finished the CMS project

--- cms.rb
+++ cms.rb
@@ -3,6 +3,8 @@ require "sinatra/reloader"
 require "tilt/erubis"
 require "redcarpet"
 require "fileutils"
+require "yaml"
+require "bcrypt"
 
 def data_path
   if ENV["RACK_ENV"] == "test"
@@ -14,6 +16,7 @@ end
 
 before do
   @files = Dir.children(data_path)
+  session[:signed_in] ||= false
 end
 
 configure do
@@ -23,6 +26,89 @@ end
 
 get "/" do
   erb :index
+end
+
+get "/sign_in" do
+  erb :login
+end
+
+get "/sign_up" do
+  erb :sign_up
+end
+
+def load_users
+  yamlfile = File.read(load_user_credentials)
+  users = YAML.load(yamlfile)
+end
+
+def load_user_credentials
+  credentials_path = if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/configuration.yaml", __FILE__)
+  else
+    File.expand_path("../configuration.yaml", __FILE__)
+  end
+end
+
+def check_login_yaml?(username, password)
+  users = load_users
+  users.keys.include?(username) && BCrypt::Password.new(users[username]) == password
+end
+
+post "/sign_in" do
+  if check_login_yaml?(params[:username], params[:password])
+    session[:signed_in] = true
+    session[:username] = params[:username]
+    session[:message] = "Welcome!"
+    redirect "/"
+  else
+    session[:message] = "Invalid Credentials"
+    status 422
+    erb :login
+  end
+end
+
+post "/sign_out" do
+  session[:signed_in] = false
+  session.delete(:username)
+  session[:message] = "You have been signed out."
+
+  redirect "/"
+end
+
+def unique_username?(new_user)
+  users = load_users
+  users.none? do |user, _|
+    user == new_user
+  end
+end
+
+def passwords_match?(password, password_confirm)
+  password == password_confirm
+end
+
+def format_into_yaml(hash)
+  str = "{"
+  hash.each do |name, password|
+    str << "#{name}: #{password},"
+  end
+  str + "}"
+end
+
+post "/sign_up" do
+  if !unique_username?(params[:username])
+    session[:message] = "Username is taken."
+    erb :sign_up
+  elsif !passwords_match?(params[:password], params[:password_confirm])
+    session[:message] = "The passwords did not match."
+    erb :sign_up
+  else
+    password = BCrypt::Password.create(params[:password])
+    users = load_users
+    users.merge!(params[:username] => password)
+    File.write(load_user_credentials, format_into_yaml(users))
+    session[:message] = "You should be able to sign in."
+    redirect "/sign_in"
+  end
 end
 
 def file_exists(name)
@@ -48,8 +134,17 @@ def load_file(name)
   end
 end
 
+def not_signed_in
+  session[:message] = "You must be signed in to do that."
+  redirect "/"
+end
+
 get "/new" do
-  erb :new
+  if session[:signed_in]
+    erb :new
+  else
+    not_signed_in
+  end
 end
 
 get "/:name" do |name|
@@ -58,9 +153,13 @@ get "/:name" do |name|
 end
 
 get "/:name/edit" do |name|
-  file_exists(name)
-  @file = load_file(name)
-  erb :edit
+  if session[:signed_in]
+    file_exists(name)
+    @file = load_file(name)
+    erb :edit
+  else
+    not_signed_in
+  end
 end
 
 EXTENSIONS = %w(.txt .md)
@@ -77,26 +176,100 @@ def no_extension(file)
 end
 
 post "/create" do
-  new_file = params[:file_name].strip
+  if session[:signed_in]
+    new_file = params[:file_name].strip
 
-  if new_file.empty?
-    session[:message] = "A name is required."
-    status 422
-    erb :new
+    if new_file.empty?
+      session[:message] = "A name is required."
+      status 422
+      erb :new
+    else
+      file = no_extension(new_file)
+      FileUtils.touch File.join(data_path, file)
+      session[:message] = "#{file} was created."
+
+      redirect "/"
+    end
   else
-    file = no_extension(new_file)
-    FileUtils.touch File.join(data_path, file)
-    session[:message] = "#{file} was created."
-
-    redirect "/"
+    not_signed_in
   end
 end
 
 post "/:name" do |name|
-  system_path = File.join(data_path, name)
-  file_exists(name)
-  File.write(system_path, params[:changes])
-  session[:message] = "#{name} has been updated."
-  redirect("/")
+  if session[:signed_in]
+    system_path = File.join(data_path, name)
+    file_exists(name)
+    File.write(system_path, params[:changes])
+    session[:message] = "#{name} has been updated."
+    redirect("/")
+  else
+    not_signed_in
+  end
+end
+
+post "/:name/delete" do |name|
+  if session[:signed_in]
+    File.delete(File.join(data_path, name))
+    session[:message] = "#{name} was deleted."
+
+    redirect "/"
+  else
+    not_signed_in
+  end
+end
+
+def is_digit?(character)
+  character =~ /[0-9]/
+end
+
+post "/:name/duplicate" do |name|
+  if session[:signed_in]
+    file = name.split(".")
+
+    matching_named_files = @files.select do |each_file|
+      each_file.include?(file[0][0...-1])
+    end
+
+    largest_num = 0
+
+    matching_named_files.each do |each_file|
+      find_num = each_file.scan(/(\d+)./).flatten[0].to_i || 0
+      if find_num > largest_num
+        largest_num = find_num
+      end
+    end
+
+    if is_digit?(file[0][-1])
+      file[0] = file[0].chars
+      file[0].pop
+      file[0] = file[0].join + (largest_num + 1).to_s
+    else
+      file[0] = file[0] + (largest_num + 1).to_s
+    end
+    file = file.join(".")
+
+    FileUtils.touch File.join(data_path, file)
+    session[:message] = "#{name} was duplicated as #{file}."
+
+    redirect "/"
+  else
+    not_signed_in
+  end
+end
+
+get "/upload/image" do
+  erb :image
+end
+
+post "/upload/image" do
+  if params[:file]
+    p params[:file][:filename]
+    # filename = params[:image][:filename]
+    # tempfile = params[:image][:tempfile]
+    # target = "../data/#{filename}"
+
+    # File.open(target, 'wb') {|f| f.write tempfile.read }
+  end
+
 end
 
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -28,6 +28,14 @@ class CMSTest < Minitest::Test
     create_document "history.txt"
   end
 
+  def session
+    last_request.env["rack.session"]
+  end
+
+  def admin_session
+    { "rack.session" => { username: "admin", signed_in: true } }
+  end
+
   def test_index
       get "/"
 
@@ -50,14 +58,8 @@ class CMSTest < Minitest::Test
     get "/random.txt"
 
     assert_equal(302, last_response.status)
-
+    assert_equal "random.txt does not exist.", session[:message]
     get last_response["Location"]
-
-    assert_equal 200, last_response.status
-    assert_includes last_response.body, "random.txt does not exist"
-
-    get "/"
-    refute_includes last_response.body, "random.txt does not exist"
   end
 
   def test_viewing_markdown_document
@@ -68,8 +70,16 @@ class CMSTest < Minitest::Test
     assert_includes last_response.body, "<h1>Ruby is...</h1>"
   end
 
+  def must_be_signed_in
+    assert_equal "You must be signed in to do that.", session[:message]
+  end
+
   def test_editing_file
     get "/changes.txt/edit"
+    assert_equal 302, last_response.status
+    must_be_signed_in
+
+    get "/changes.txt/edit", {}, admin_session
 
     assert_equal 200, last_response.status
     assert_includes last_response.body, "<textarea"
@@ -78,12 +88,12 @@ class CMSTest < Minitest::Test
 
   def test_updating_file
     post "/changes.txt", changes: "New content"
+    must_be_signed_in
+
+    post "/changes.txt", {changes: "New content"}, admin_session
 
     assert_equal 302, last_response.status
-
-    get last_response["Location"]
-
-    assert_includes last_response.body, "changes.txt has been updated"
+    assert_equal "changes.txt has been updated.", session[:message]
 
     get "/changes.txt"
     assert_equal 200, last_response.status
@@ -92,28 +102,29 @@ class CMSTest < Minitest::Test
 
   def test_new
     get "/new"
+    assert_equal 302, last_response.status
+    must_be_signed_in
 
+    get "/new", {}, admin_session
     assert_equal 200, last_response.status
     assert_includes last_response.body, %q(<button type="submit)
     assert_includes last_response.body, %q(id="doc_name)
   end
 
   def test_new_empty_file_name
-    post "/create", file_name: ""
+    post "/create", {file_name: ""}, admin_session
 
     assert_equal 422, last_response.status
     assert_includes last_response.body, %q(A name is required.</p>)
   end
 
   def new_file_naming(name)
-    post "/create", file_name: name
+    post "/create", {file_name: name}
+    must_be_signed_in
 
+    post "/create", {file_name: name}, admin_session
     assert_equal 302, last_response.status
-
-    get last_response["Location"]
-
-    assert_equal 200, last_response.status
-    assert_includes last_response.body, %q(hello.txt was created)
+    assert_equal "hello.txt was created.", session[:message]
 
     get "/"
     assert_includes last_response.body, "hello.txt"
@@ -129,6 +140,92 @@ class CMSTest < Minitest::Test
 
   def test_new_file
    new_file_naming("hello.txt")
+  end
+
+  def test_deleteing_file
+    get "/"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "Delete</button>"
+
+    post "/changes.txt/delete"
+    must_be_signed_in
+
+    post "/changes.txt/delete", {}, admin_session
+    assert_equal 302, last_response.status
+    assert_equal "changes.txt was deleted.", session[:message]
+
+    get "/"
+    refute_includes last_response.body, %q(href="/changes.txt)
+  end
+
+  def test_signed_out
+    get "/"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "Sign In"
+  end
+
+  def test_sign_in_route
+    get "/sign_in"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, %q(name="username)
+    assert_includes last_response.body, %q(name="password)
+    assert_includes last_response.body, "Sign In</button>"
+  end
+
+  def test_signing_in_success_as_admin
+    post "/sign_in", username: "natalie", password: "password"
+    assert_equal 302, last_response.status
+    assert_equal "Welcome!", session[:message]
+    assert_equal "natalie", session[:username]
+
+    get last_response["location"]
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "Welcome!"
+    assert_includes last_response.body, %q(Sign out</button>)
+
+    get "/"
+    refute_includes last_response.body, "Welcome!"
+  end
+
+  def test_signing_in_success_as_user
+    post "/sign_in", username: "natalie", password: "password"
+    assert_equal 302, last_response.status
+    assert_equal "Welcome!", session[:message]
+    assert_equal "natalie", session[:username]
+
+    get last_response["location"]
+    assert_includes last_response.body, "Welcome!"
+  end
+
+  def test_signing_in_fail
+    post "/sign_in", username: "blah", password: "la"
+
+    assert_equal 422, last_response.status
+    assert_nil session[:username]
+    assert_includes last_response.body, "Invalid Credentials"
+    assert_includes last_response.body, %q(value=blah)
+  end
+
+  def test_signing_out
+    get "/", {}, admin_session
+
+    post "/sign_out"
+    assert_equal 302, last_response.status
+    assert_equal "You have been signed out.", session[:message]
+
+    get last_response["location"]
+    assert_nil session[:username]
+    assert_includes last_response.body, "Sign In"
+  end
+
+  def test_sets_session_value
+    get "/"
+
+    assert_equal false, session["signed_in"]
   end
 
   def teardown
